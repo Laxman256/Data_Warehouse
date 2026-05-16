792b19cc78969590e509b5ac95bd87637b4657cc
Laxman256 <49369290+Laxman256@users.noreply.github.com>
2026-05-16 19:54:07 -0400

    Add files via upload

---
+++ scripts/Silver/Silver_layerdata_injection_final.sql
@@ -0,0 +1,145 @@
+PRINT '>>Truncating Table: silver.crm_cust_info';
+TRUNCATE TABLE silver.crm_cust_info;
+PRINT'>>Inseritng Data Into silver.crm_cust_info'
+INSERT INTO silver.crm_cust_info(
+	cst_id ,            
+    cst_key,           
+    cst_firstname ,      
+    cst_lastname,  
+	cst_gndr,  
+    cst_marital_status,              
+    cst_create_date     
+)
+
+SELECT cst_id, cst_key,
+TRIM(cst_firstname) as cst_firstname,
+TRIM(cst_lastname) as cst_lastname,
+CASE 
+	WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	ELSE 'n/a'  
+END cst_gndr,
+CASE 
+	WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	ELSE 'n/a'
+END cst_marital_status,
+cst_create_date
+from( 
+Select *, 
+ROW_NUMBER() over (partition by cst_id order by cst_create_date desc) as flag_last
+from bronze.crm_cust_info
+where cst_id is not null
+)t where flag_last = 1;
+
+PRINT '>>Truncating Table: silver.crm_prd_info';
+TRUNCATE TABLE silver.crm_prd_info;
+PRINT'>>Inseritng Data Into silver.crm_prd_info'
+INSERT INTO silver.crm_prd_info(
+	prd_id,     
+	cat_id,	
+	prd_key,		
+	prd_nm,      
+    prd_cost,    
+    prd_line,    
+    prd_start_dt, 
+    prd_end_dt   
+	
+)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) as prd_key,
+prd_nm,
+ISNULL(prd_cost,0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+	when 'M' then 'Mountain'
+	when 'R' then 'Road'
+	When 'S' then 'Ohter Sales'
+	When 'T' then 'Touring'
+	else 'n/a' 
+	end as prd_line,
+CAST(prd_start_dt as DATE) as prd_start_dt,
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) as prd_end_dt
+FROM bronze.crm_prd_info;
+
+PRINT '>>Truncating Table: silver.crm_sales_details';
+TRUNCATE TABLE silver.crm_sales_details;
+PRINT'>>Inseritng Data Into silver.crm_sales_details'
+INSERT INTO silver.crm_sales_details (
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN Null
+	ELSE CAST(CAST(sls_order_dt as varchar)as DATE)
+	END as sls_order_dt,
+CASE WHEN sls_ship_dt = 0 or LEN(sls_ship_dt) != 8 THEN Null
+	ELSE CAST(CAST(sls_ship_dt as varchar)as DATE)
+	END as sls_ship_dt,
+CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) != 8 THEN Null
+	ELSE CAST(CAST(sls_due_dt as varchar)as DATE)
+	END as sls_due_dt,
+CASE
+WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity*ABS(sls_price)
+		THEN sls_quantity*ABS(sls_price)
+		ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0 
+		THEN sls_sales/NULLIF(sls_quantity,0)
+		ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details
+
+PRINT '>>Truncating Table: silver.erp_cust_az12';
+TRUNCATE TABLE silver.erp_cust_az12;
+PRINT'>>Inseritng Data Into silver.erp_cust_az12'
+INSERT INTO silver.erp_cust_az12(cid,bdate,gen)
+
+Select 
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4, len(cid))
+		else cid
+end as cid,
+CASE WHEN bdate > GETDATE() THEN NULL
+	ELSE bdate
+end bdate,
+CASE WHEN UPPER(TRIM(gen)) in ('F','FEMALE') then 'Female'
+	WHEN UPPER(TRIM(gen)) in ('M','MALE') then 'Male'
+	ELSE 'n/a'
+end AS gen
+From bronze.erp_cust_az12
+
+PRINT '>>Truncating Table: silver.erp_loc_a101';
+TRUNCATE TABLE silver.erp_loc_a101;
+PRINT'>>Inseritng Data Into silver.erp_loc_a101'
+INSERT INTO silver.erp_loc_a101(cid,cntry)
+SELECT
+REPLACE(cid,'-','') as cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM(cntry)
+END cntry
+FROM bronze.erp_loc_a101
+
+PRINT '>>Truncating Table: silver.erp_px_cat_g1v2';
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+PRINT'>>Inseritng Data Into silver.erp_px_cat_g1v2'
+INSERT INTO silver.erp_px_cat_g1v2(id,cat,subcat,maintenance)
+SELECT
+id,
+cat,
+subcat,
+maintenance
+from bronze.erp_px_cat_g1v2
